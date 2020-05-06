5f99bdd16e106abf0c13b3b54673ad31aee73031
unknown <you@example.com>
2020-05-06 16:40:36 +0400

    medical

--- src/Footer/footer.js
+++ src/Footer/footer.js
@@ -57,32 +57,34 @@ export default class Footer extends Component{
                     </Container>
                     <Container className='footContact'>
                         <Row>
-                            <Col sm={8}>
+                            <Col sm={10}>
                                 <Container className='contactFooter'>
                                     <Row>
-                                        <Col md="auto" className='call'>
+                                        <Col md="auto" xl={3} className='call'>
                                             <div>
                                                 <a href='tel:+8189379977'>
                                                     <i className="fas fa-phone fa-flip-horizontal"></i><span className='entTitle'>(818) 937-9977</span>
                                                 </a>
                                             </div>
                                         </Col>
-                                        <Col md="auto" className='em'>
+                                        <Col md="auto" xl={3} className='em'>
                                             <div>
                                                 <a href='mailto:Info@neighborhoodch.com'>
                                                     <i className="fas fa-envelope"></i><span className='entTitle'>Info@neighborhoodch.com</span>
                                                 </a>
                                             </div>
                                         </Col>
-                                        <Col md="auto" className='ad'>
+                                        <Col md="auto" xl={6} className='ad'>
                                             <div>
-                                                <i className="fas fa-map-marker-alt"></i><span className='entTitle'> 17418 Chatsworth St. Granada Hills, Los Angeles, CA, USA</span>
+                                                <a target="_blank"  href={"https://www.google.com/maps/place/17418+Chatsworth+St,+Granada+Hills,+CA+91344,+%D5%84%D5%AB%D5%A1%D6%81%D5%B5%D5%A1%D5%AC+%D5%86%D5%A1%D5%B0%D5%A1%D5%B6%D5%A3%D5%B6%D5%A5%D6%80/@34.2645171,-118.5160253,17z/data=!3m1!4b1!4m5!3m4!1s0x80c29aea4169214d:0x30da89ac5af8d035!8m2!3d34.2645127!4d-118.5138366"}>
+                                                    <i className="fas fa-map-marker-alt"></i><span className='entTitle'> 17418 Chatsworth St. Granada Hills, Los Angeles, CA, 91205</span>
+                                                </a>
                                             </div>
                                         </Col>
                                     </Row>
                                 </Container>
                             </Col>
-                            <Col sm={4}>
+                            <Col sm={2}>
                                 <a target="_blank" href='https://www.facebook.com/' rel="noopener noreferrer">
                                     <i  className="fab fa-facebook-square" ></i>
                                 </a>
--- src/nav/Nav.js
+++ src/nav/Nav.js
@@ -33,9 +33,13 @@ import Col from "react-bootstrap/Col";
                               </Col>
 
                               <Col md="auto">
-                                  <div>
-                                      |<i className="fas fa-map-marker-alt"></i><span> 17418 Chatsworth St. Granada Hills, Los Angeles, CA, USA</span>
+                                  {/*<a target="_blank"  href={"https://www.google.com/maps/place/17418+Chatsworth+St,+Granada+Hills,+CA+91344,+%D5%84%D5%AB%D5%A1%D6%81%D5%B5%D5%A1%D5%AC+%D5%86%D5%A1%D5%B0%D5%A1%D5%B6%D5%A3%D5%B6%D5%A5%D6%80/@34.2645171,-118.5160253,17z/data=!3m1!4b1!4m5!3m4!1s0x80c29aea4169214d:0x30da89ac5af8d035!8m2!3d34.2645127!4d-118.5138366"}>*/}
+                                  <div>|
+                                      <a target="_blank"  href={"https://www.google.com/maps/place/17418+Chatsworth+St,+Granada+Hills,+CA+91344,+%D5%84%D5%AB%D5%A1%D6%81%D5%B5%D5%A1%D5%AC+%D5%86%D5%A1%D5%B0%D5%A1%D5%B6%D5%A3%D5%B6%D5%A5%D6%80/@34.2645171,-118.5160253,17z/data=!3m1!4b1!4m5!3m4!1s0x80c29aea4169214d:0x30da89ac5af8d035!8m2!3d34.2645127!4d-118.5138366"}>
+                                            <i className="fas fa-map-marker-alt"></i><span> 17418 Chatsworth St. Granada Hills, Los Angeles, CA, USA</span>
+                                       </a>
                                   </div>
+                                  {/*</a>*/}
                               </Col>
                           </Row>
                      </Container>
